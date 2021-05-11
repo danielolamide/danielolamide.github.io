0fd547f70a08dc9abe83fcf05b511ffd65f1e6b9
dannyolamide <dannyolamide@gmail.com>
2021-05-11 19:45:42 +0300

    added mobile menus and tailwindcss

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
-function openInNewTab(url){
-    var win = window.open(url, '_blank')
-    win.focus();
+function toggleMenu() {
+	var menu = document.getElementById("menu");
+	var closeMenu = document.getElementById("close-menu");
+	var openMenu = document.getElementById("open-menu");
+	menu.classList.toggle("hidden");
+	if (!menu.classList.contains("hidden")) {
+		openMenu.classList.toggle("hidden");
+		closeMenu.classList.toggle("hidden");
+	} else {
+		closeMenu.classList.toggle("hidden");
+		openMenu.classList.toggle("hidden");
+	}
 }
